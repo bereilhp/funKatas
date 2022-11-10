c0de5c037bbf3283fb23f322fd7777cbeeabe670
PedroB <bereilhp@gmail.com>
2022-11-10 22:20:29 +0100

    messi kata

---
+++ messiGoals/messi.js
@@ -0,0 +1,12 @@
+let laLigaGoals = 43;
+let championsLeagueGoals = 10;
+let CopaDelRey = 5;
+
+let totalGoals = laLigaGoals + championsLeagueGoals + CopaDelRey;
+
+function goals(){
+    return laLigaGoals + championsLeagueGoals + CopaDelRey;
+}
+
+module.exports = goals;
+ 
---
+++ messiGoals/messi.test.js
@@ -0,0 +1,6 @@
+const goals = require("./messi.js");
+
+test ("Check if goals are added correctly", () => {
+    expect(goals()).toBe(58);
+})
+
