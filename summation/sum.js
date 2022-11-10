3d08796f774d6f6f54ac5c2115692d6f9bb1792a
PedroB <bereilhp@gmail.com>
2022-11-10 22:46:31 +0100

    summation kata

--- summation/sum.js
+++ summation/sum.js
@@ -1,1 +1,9 @@
+function summation(num){
+    let ans = 0;
+    for(let i = 1; i <= num; i++){
+        ans = ans + i;
+    }
+    return ans;
+}
 
+module.exports = summation;
--- summation/sum.test.js
+++ summation/sum.test.js
@@ -1,1 +1,9 @@
+const summation = require("./sum")
 
+test ("Sum from 1 to 2", () => {
+    expect(summation(2)).toBe(3);
+});
+
+test ("Sum from 1 to 8", () => {
+    expect(summation(8)).toBe(36);
+});
