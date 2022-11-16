61e82393305a4d7fc3ac43d0b3ab7f218ffb2af3
PedroB <bereilhp@gmail.com>
2022-11-16 11:32:16 +0100

    highestLowest tests

---
+++ highestAndLowest/highLow.js
@@ -0,0 +1,10 @@
+function highLow(str){
+    let x = str.split(' '); // split
+    let result = x.map(i=>Number(i)); // pass them as numbers into an array 
+    let arrSort = result.sort((a, b) => a - b); // sort the array 
+    let big = arrSort[arrSort.length - 1]; // return the last number from the array (biggest)
+    let small = arrSort[0]; // return the first  number from the array (smallest)
+    return `${big} ${small}`;
+}
+
+module.exports = highLow;
