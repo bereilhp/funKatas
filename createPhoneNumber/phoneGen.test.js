1538b69b41c65d5f2f1e5c877e70eaad00fb22ba
PedroB <bereilhp@gmail.com>
2022-11-19 21:05:38 +0100

    Phone Number Generator Test

--- createPhoneNumber/phoneGen.test.js
+++ createPhoneNumber/phoneGen.test.js
@@ -1,4 +1,4 @@
 const phoneGenerator = require('./phoneGen');
 test ("Dummy", () => {
-    expect(phoneGenerator([])).toEqual([]);
+    expect(phoneGenerator([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])).toEqual("(123) 456-7890");
 })
