7629eada62630580f1758875e7f843f8229e24ce
PedroB <bereilhp@gmail.com>
2022-11-19 21:29:05 +0100

    Even and Odd test from readme

--- findParityOutlier/outlier.test.js
+++ findParityOutlier/outlier.test.js
@@ -6,3 +6,11 @@ test ("Oddy", () => {
 test ("Eveny", () => {
     expect(eveny([1,2,3])).toBe(2);
 })
+
+test ("Oddy README", () => {
+    expect(oddy([2, 4, 0, 100, 4, 11, 2602, 36])).toBe(11);
+})
+
+test ("Eveny README", () => {
+    expect(eveny([160, 3, 1719, 19, 11, 13, -21])).toBe(160);
+})
