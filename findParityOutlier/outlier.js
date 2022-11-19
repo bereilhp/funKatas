b650ed0e30543c00950189e540650fcc0d703da9
PedroB <bereilhp@gmail.com>
2022-11-19 21:20:05 +0100

    if odd test

--- findParityOutlier/outlier.js
+++ findParityOutlier/outlier.js
@@ -1,5 +1,11 @@
 function oddy(a) {
-    return a
+    let x = 0 
+    for(let i = 0; i < a.length; i++) {
+        if(a[i] % 2 == 1){
+            x = a[i]
+        } 
+    }
+    return x
 }
 
 module.exports = oddy;
--- findParityOutlier/outlier.test.js
+++ findParityOutlier/outlier.test.js
@@ -1,4 +1,4 @@
 const oddy = require("./outlier.js");
 test ("Dummy", () => {
-    expect(oddy("2")).toBe("2");
+    expect(oddy([2,3,4])).toBe(3);
 })
