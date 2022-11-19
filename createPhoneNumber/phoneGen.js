f830c749500cf924eff1787da9f225d9319decf5
PedroB <bereilhp@gmail.com>
2022-11-19 21:07:20 +0100

    Phone Gen Done

--- createPhoneNumber/phoneGen.js
+++ createPhoneNumber/phoneGen.js
@@ -1,5 +1,5 @@
 function phoneGenerator(a){
-    return `(${a[0]}${a[1]}${a[2]}) ${a[3]}${a[4]}${a[5]}-${a[6]}${a[7]}${a[8]}${a[9]}` ;
+    return `(${a[0]}${a[1]}${a[2]}) ${a[3]}${a[4]}${a[5]}-${a[6]}${a[7]}${a[8]}${a[9]}`;
 }
 
 module.exports = phoneGenerator;
