f1ccf663855f035bfc8da0e68b47c9e642502793
PedroB <bereilhp@gmail.com>
2022-11-19 21:25:39 +0100

    eveny function with test

--- findParityOutlier/outlier.js
+++ findParityOutlier/outlier.js
@@ -8,4 +8,14 @@ function oddy(a) {
     return x
 }
 
-module.exports = oddy;
+function eveny(a) {
+    let x = 0 
+    for(let i = 0; i < a.length; i++) {
+        if(a[i] % 2 == 0){
+            x = a[i]
+        } 
+    }
+    return x
+}
+
+module.exports = { oddy, eveny };
--- findParityOutlier/outlier.test.js
+++ findParityOutlier/outlier.test.js
@@ -1,4 +1,8 @@
-const oddy = require("./outlier.js");
-test ("Dummy", () => {
+const { oddy, eveny } = require("./outlier.js");
+test ("Oddy", () => {
     expect(oddy([2,3,4])).toBe(3);
+})
+
+test ("Eveny", () => {
+    expect(eveny([1,2,3])).toBe(2);
 })
