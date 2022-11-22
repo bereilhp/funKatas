a07c83d25a4ac212f7e7dd670c852a1e053139a7
PedroB <bereilhp@gmail.com>
2022-11-22 15:19:20 +0100

    bowlingScoreSpare function

--- tenPinBowling/bowling.js
+++ tenPinBowling/bowling.js
@@ -8,7 +8,17 @@ function bowlingScoreSimple(a){
     return (result.map(Number).reduce((a,b) => a + b,0));
 }
 
-function bowlingScoreSpare(){
+function bowlingScoreSpare(a){
+    const arr = a.split(" "); //.map(Number);
+    let result = []
+    for(let i = 0; i <= (arr.length - 1); i++){
+            result.push(arr[i].charAt(0)); 
+            result.push(arr[i].charAt(1));
+    }
+    return (result.map(Number).reduce((a,b) => a + b,0));
+}
+
+function bowlingScoreStrike(a){
     const arr = a.split(" "); //.map(Number);
     let result = []
     for(let i = 0; i <= (arr.length - 1); i++){
@@ -22,18 +32,4 @@ function bowlingScoreSpare(){
     return (result.map(Number).reduce((a,b) => a + b,0));
 }
 
-function bowlingScoreStrike(){
-    const arr = a.split(" "); //.map(Number);
-    let result = []
-    for(let i = 0; i <= (arr.length - 1); i++){
-        if(arr[i] == "X"){
-            result.push(arr[i].charAt(0)); 
-        }else {
-            result.push(arr[i].charAt(0)); 
-            result.push(arr[i].charAt(1));
-        }
-    }
-    return (result.map(Number).reduce((a,b) => a + b,0));
-}
-
-module.exports = bowlingScoreSimple;
+module.exports = { bowlingScoreSimple, bowlingScoreSpare } ;
--- tenPinBowling/bowling.test.js
+++ tenPinBowling/bowling.test.js
@@ -1,5 +1,9 @@
-const bowlingScoreSimple = require("./bowling");
+const { bowlingScoreSimple, bowlingScoreSpare } = require("./bowling");
 
-test ("Dummy Test", () => {
+test ("All numbers no spare or strike", () => {
     expect(bowlingScoreSimple("52 45 63 54 62 71 81 72 54 34")).toEqual(84);
 });
+
+test ("All numbers and spares", () => {
+    expect(bowlingScoreSpare("5/ 45 63 54 62 71 81 72 54 34")).toEqual(91);
+})
