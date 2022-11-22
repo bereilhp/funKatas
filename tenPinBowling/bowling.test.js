9ec4939fe3172410e56d244768b32b8892609b6a
PedroB <bereilhp@gmail.com>
2022-11-22 15:51:46 +0100

    All zero bowling test

--- tenPinBowling/bowling.test.js
+++ tenPinBowling/bowling.test.js
@@ -4,6 +4,10 @@ test ("All numbers no spare or strike", () => {
     expect(bowlingScoreSimple("52 45 63 54 62 71 81 72 54 34")).toEqual(84);
 });
 
+test ("All number 0", () => {
+    expect(bowlingScoreSimple("00 00 00 00 00 00 00 00 00 00")).toEqual(0);
+});
+
 test ("All numbers and spares", () => {
     expect(bowlingScoreSpare("5/ 45 63 54 62 71 81 72 54 34")).toEqual(91);
 })
