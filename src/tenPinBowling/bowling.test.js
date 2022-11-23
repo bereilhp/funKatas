934f276147bd976b8d4e20d322196e06fac81c0e
PedroB <bereilhp@gmail.com>
2022-11-23 19:54:36 +0100

    Spare test for multiple spares

--- src/tenPinBowling/bowling.test.js
+++ src/tenPinBowling/bowling.test.js
@@ -11,3 +11,7 @@ test ("All number 0", () => {
 test ("All numbers and spares", () => {
     expect(bowlingScoreSpare("6/ 45 63 54 62 71 81 72 54 34")).toBe(91);
 })
+
+test ("Multiple spares", () => {
+    expect(bowlingScoreSpare("72 54 6/ 6/ 72 72 53 54 63 45")).toBe(104)
+});
