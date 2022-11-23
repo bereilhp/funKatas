37595aa0c41ec8355c30eef375583949e7fea4f8
PedroB <bereilhp@gmail.com>
2022-11-23 20:18:39 +0100

    Spare with extra ball function and test

--- src/tenPinBowling/bowling.js
+++ src/tenPinBowling/bowling.js
@@ -10,7 +10,7 @@ function bowlingScoreSimple(a){
 
 function bowlingScoreSpare(a){
     const arr = a.split(" "); //.map(Number);
-    let result = []
+    let result = [];
     for(let i = 0; i <= (arr.length - 1); i++){
         if(arr[i].charAt(1) == '/'){
             result.push(arr[i].charAt(0)); 
@@ -24,6 +24,24 @@ function bowlingScoreSpare(a){
     }
     return (result.map(Number).reduce((a,b)=> a+b,0));
 }
+
+function bowlingScoreSpareWithExtra(a){
+    const arr = a.split(" "); //.map(Number);
+    let result = [];
+    for(let i = 0; i <= (arr.length - 1); i++){
+        if(arr[i].charAt(1) == '/'){
+            result.push(arr[i].charAt(0)); 
+            let diferencia = 10 - arr[i].charAt(0);
+            result.push(diferencia.toString()); 
+            result.push(arr[i+1].charAt(0));
+        } else {
+            result.push(arr[i].charAt(0)); 
+            result.push(arr[i].charAt(1));
+        }
+    }
+    return (result.map(Number).reduce((a,b)=> a+b,0) - result[result.length - 2]);
+}
+
 
 function bowlingScoreStrike(a){
     const arr = a.split(" "); //.map(Number);
@@ -42,4 +60,4 @@ function bowlingScoreStrike(a){
     return (result.map(Number).reduce((a,b) => a + b,0));
 }
 
-module.exports = { bowlingScoreSimple, bowlingScoreSpare } ;
+module.exports = { bowlingScoreSimple, bowlingScoreSpare, bowlingScoreSpareWithExtra } ;
--- src/tenPinBowling/bowling.test.js
+++ src/tenPinBowling/bowling.test.js
@@ -1,4 +1,4 @@
-const { bowlingScoreSimple, bowlingScoreSpare } = require("./bowling");
+const { bowlingScoreSimple, bowlingScoreSpare, bowlingScoreSpareWithExtra } = require("./bowling");
 
 test ("All numbers no spare or strike", () => {
     expect(bowlingScoreSimple("52 45 63 54 62 71 81 72 54 34")).toEqual(84);
@@ -14,4 +14,8 @@ test ("All numbers and spares", () => {
 
 test ("Multiple spares", () => {
     expect(bowlingScoreSpare("72 54 6/ 6/ 72 72 53 54 63 45")).toBe(104)
+});
+
+test ("Multiple spares and extra end", () => {
+    expect(bowlingScoreSpareWithExtra("72 54 6/ 6/ 72 72 53 54 63 4/ 2")).toBe(107)
 });
