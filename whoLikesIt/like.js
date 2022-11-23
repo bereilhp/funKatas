4626b72d9e1651cc3ae54d5bc4f8f49f4bff1521
PedroB <bereilhp@gmail.com>
2022-11-23 06:20:32 +0100

    who likes it other if

--- whoLikesIt/like.js
+++ whoLikesIt/like.js
@@ -4,10 +4,11 @@ function likes(a){
     } else if(a.length == 1){
         return `${a[0]} likes this`;
     } else if(a.length == 2){
-        return `${a[0]} and ${a[1]} likes this`;
+        return `${a[0]} and ${a[1]} like this`;
     } else if(a.length == 3){
-        return `${a[0]} , ${a[1]} and ${a[2]} likes this`;
-    }
+        return `${a[0]}, ${a[1]} and ${a[2]} like this`;
+    } else 
+        return `${a[0]}, ${a[1]} and ${a.length - 2} others like this`;
 }
 
 module.exports = likes;
--- whoLikesIt/like.test.js
+++ whoLikesIt/like.test.js
@@ -8,5 +8,9 @@ test ("Peter likes it", () => {
 })
 
 test ("Peter and Mike like it", () => {
-    expect(likes(["Peter", "Mike"])).toEqual("Peter and Mike likes this");
+    expect(likes(["Peter", "Mike"])).toEqual("Peter and Mike like this");
+})
+
+test ("4 like it", () => {
+    expect(likes(["Peter", "Mike", "Juan", "Messi"])).toEqual("Peter, Mike and 2 others like this");
 })
