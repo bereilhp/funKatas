50845390823b1d38e998a70e2a2c5c567f065af9
PedroB <bereilhp@gmail.com>
2022-11-30 17:51:26 +0100

    Test length, letter and U,I,O,Ñ case

--- src/dni/dniCalc.js
+++ src/dni/dniCalc.js
@@ -1,9 +1,23 @@
 function dni(a){ 
     if(a.length == 9){
-        return a.length;
-    } else 
-        return "Incorrect format"
+        for(let i = 0; i < (a.length-1); i++){
+            if(typeof(a[i]) == 'number'){
+                continue;
+            } else
+                return "The ID should only be numbers and the last a letter";
+        }
+    
+        if(a[8] == "U" || (a[8] == "I") || (a[8] == "O") || (a[8] == "Ñ")){
+            return "ID should not contain this letter"; 
+        } else      
+            return a.length;
 
+    } else 
+        return "The ID should only be 9 characters long";
 }
 
-module.exports = dni;
+function dniLetra(a){
+    
+}
+
+module.exports = { dni, dniLetra };
--- src/dni/dniCalc.test.js
+++ src/dni/dniCalc.test.js
@@ -1,4 +1,28 @@
-const dni = require("./dniCalc.js");
+const { dni, dniLetra } = require("./dniCalc.js");
 test ("Test Dni Size", () => {
-    expect(dni([1,2,3,4,5,6,7,8,9])).toBe(9);
-})
+    expect(dni([1,2,3,3,5,6,7,8,"T"])).toBe(9);
+});
+
+test ("Test Dni Size More", () => {
+    expect(dni([1,2,3,3,5,6,7,8,"T",10])).toBe("The ID should only be 9 characters long");
+});
+
+test ("Test Dni with more letters", () => {
+    expect(dni([1,2,3,3,5,"R",7,8,"T"])).toBe("The ID should only be numbers and the last a letter");
+});
+
+test ("Test Dni with Ñ", () => {
+    expect(dni([1,2,3,3,5,6,7,8,"Ñ"])).toBe("ID should not contain this letter");
+});
+
+test ("Test Dni with U", () => {
+    expect(dni([1,2,3,3,5,6,7,8,"U"])).toBe("ID should not contain this letter");
+});
+
+test ("Test Dni with I", () => {
+    expect(dni([1,2,3,3,5,6,7,8,"I"])).toBe("ID should not contain this letter");
+});
+
+test ("Test Dni with O", () => {
+    expect(dni([1,2,3,3,5,6,7,8,"O"])).toBe("ID should not contain this letter");
+});
