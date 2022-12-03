410630ca0df743d062bed9250b9adbbca1b53416
PedroB <bereilhp@gmail.com>
2022-12-03 19:32:34 +0100

    Find odd function and test

--- src/findTheOddInt/odd_int.js
+++ src/findTheOddInt/odd_int.js
@@ -1,5 +1,15 @@
+const counts = {};
+let arr = []
 const findOddy = (input) => {
-    return input
+    input.forEach(function (x) { counts[x] = (counts[x] || 0) + 1; });
+    //console.log(Object.keys(counts).length); // print size of an object
+    //console.log(Object.keys(counts)); // print size of an object
+    for(let i in counts){
+       //console.log("Key = " + i + " " + "Value = " + counts[i]); 
+       if(counts[i] % 2  == 1){
+        return Number(i)
+       }
+    }
 }
 
 module.exports = findOddy;
--- src/findTheOddInt/odd_int.test.js
+++ src/findTheOddInt/odd_int.test.js
@@ -1,4 +1,10 @@
 const findOddy = require("./odd_int"); 
-test ("Dummy test", () => {
-    expect(findOddy([1,2,3])).toEqual([1, 2, 3]);
-})
+test ("Testing with simple example", () => {
+    expect(findOddy([1,1,2,3,3,3,3])).toEqual(2);
+});
+
+test ("Testing with readme examples", () => {
+    expect(findOddy([0,1,0,1,0])).toBe(0);
+});
+
+
