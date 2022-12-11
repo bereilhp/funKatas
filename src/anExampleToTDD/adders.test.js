2454f9e12c3da65429f6fa5935a5883d6e979d7a
PedroB <bereilhp@gmail.com>
2022-12-11 18:33:30 +0100

    jest types autocomplete

--- src/anExampleToTDD/adders.test.js
+++ src/anExampleToTDD/adders.test.js
@@ -1,7 +1,9 @@
 const add = require("./adders.js")
 test ("Example", () => {
     expect("test").toBe("test");
-})
+});
+
+
 
 test("Test #1 - add two numbers",() => {
     expect(add(1,2)).toBe(3);
