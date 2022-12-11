17cc0f08da2bce9865fd884b3e515279808d6a84
PedroB <bereilhp@gmail.com>
2022-12-11 18:15:42 +0100

    anExampleToTDD

---
+++ src/anExampleToTDD/README.md
@@ -0,0 +1,5 @@
+# Description 
+
+## Example of TDD 
+
+Create a function that adds to numbers use TDD.
---
+++ src/anExampleToTDD/adders.test.js
@@ -0,0 +1,8 @@
+const add = require("./adders.js")
+test ("Example", () => {
+    expect("test").toBe("test");
+})
+
+test("Test #1 - add two numbers",() => {
+    expect(add(1,2)).toBe(3);
+})
