d0187f26501580d72ea80debc1beb3653e284194
PedroB <bereilhp@gmail.com>
2022-12-13 16:09:02 +0100

    square kata finished

--- src/squareEveryDigit/square.js
+++ src/squareEveryDigit/square.js
@@ -1,5 +1,12 @@
 function square(num){
-    return num**2
+    let x = [...num+''].map(n=>+n);
+    let ans = x.map(function(num){
+        return num**2
+    });
+   
+    let joined = ans.join("");
+    let nums = Number(joined);
+    return nums;
 }
 
 module.exports = square;
--- src/squareEveryDigit/square.test.js
+++ src/squareEveryDigit/square.test.js
@@ -11,3 +11,15 @@ test ("Square a number with two values", () => {
     expect(square(50)).toBe(250);
 });
 
+test ("Square a number with two values", () => {
+    expect(square(68)).toBe(3664);
+});
+
+test ("Square a number with two values", () => {
+    expect(square(5584)).toBe(25256416);
+});
+test ("Square a number with ten values", () => {
+    expect(square(5555555555555)).toBe(25252525252525252525252525);
+});
+
+
