39af34d603f0445f78ddc1b240507d29bdde2795
PedroB <bereilhp@gmail.com>
2022-12-13 15:25:07 +0100

    Square Test 2

--- src/squareEveryDigit/square.test.js
+++ src/squareEveryDigit/square.test.js
@@ -1,4 +1,8 @@
 const square = require("./square.js")
 test ("Prove if function exist", () => {
     expect(typeof(square)).toBe("function");
+});
+
+test ("Square a number", () => {
+    expect(square(5)).toBe(25);
 })
