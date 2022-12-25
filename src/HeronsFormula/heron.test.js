9aafd0ab62b1c874208d342cb7b4a39ea7f0a409
PedroB <bereilhp@gmail.com>
2022-12-25 13:44:04 -0300

    en

--- src/HeronsFormula/heron.test.js
+++ src/HeronsFormula/heron.test.js
@@ -7,6 +7,6 @@ test ("#1 Test - Calculate S", () => {
     expect(heron.calculateS(1,2,2)).toBe(2.5)
 })
 
-test ("#2 Test - Calculate S", () => {
+test ("#2 Test - Calculate using Heron Formula", () => {
     expect(heron.calculate(1,2,2)).toBe(0.97)
 })
