bf2129c72567076454aa72674430319b6f81c109
PedroB <bereilhp@gmail.com>
2023-02-01 09:00:41 +0100

    Largest Element Test #2 and fixed sorting function

--- src/largestElement/bigInt.js
+++ src/largestElement/bigInt.js
@@ -7,7 +7,7 @@ num = {};
  * @returns New list containing n elements but those n elements the largest/biggest of the list
  */
 num.biggestInt = function(n,array){
-    let orderedArray = array.sort().reverse();
+    let orderedArray = array.sort((a, b) => a - b).reverse();
     let newArray = orderedArray.slice(0,n).reverse(); 
     return newArray;
 }
--- src/largestElement/bigInt.test.js
+++ src/largestElement/bigInt.test.js
@@ -6,3 +6,7 @@ test ("Example", () => {
 test ("Test #1", () => {
     expect(num.biggestInt(2,[7,6,5,4,3,2,1])).toEqual([6,7]);
 });
+
+test ("Test #2", () => {
+    expect(num.biggestInt(2,[10,9,8,7,6,5,4,3,2,1])).toEqual([9,10]);
+});
