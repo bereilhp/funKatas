8f7884c4e0fc551390422ba2f8ef89bbf5f47c2c
PedroB <bereilhp@gmail.com>
2023-02-01 08:48:44 +0100

    Largest Element Test #1

---
+++ src/largestElement/bigInt.js
@@ -0,0 +1,14 @@
+num = {};
+
+/**
+ * 
+ * @param {int} n Output the n largest elements from the list 
+ * @param {list} array List of different numbers
+ * @returns 
+ */
+num.biggestInt = function(n,array){
+    return array
+}
+
+module.exports = num;
+
--- src/largestElement/bigInt.test.js
+++ src/largestElement/bigInt.test.js
@@ -1,3 +1,8 @@
+const num = require("./bigInt")
 test ("Example", () => {
     expect("test").toBe("test");
-})
+});
+
+test ("Test #1", () => {
+    expect(num.biggestInt(1,2)).toBe(2);
+});
