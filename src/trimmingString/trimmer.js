261ad2c013b464794979268f5ed047127d8c2c80
PedroB <bereilhp@gmail.com>
2023-02-05 18:44:58 +0100

    Trimmer working algorithm with tests

--- src/trimmingString/trimmer.js
+++ src/trimmingString/trimmer.js
@@ -3,8 +3,18 @@ trim = {
 };
 
 trim.trimmerString = function (text, cutter) {
-    return text + cutter;
-    
+    let textSize = text.length
+    if(textSize > cutter){ // Means that text is bigger than cutter there will be three dots
+        if(cutter > 3){
+            let newText = text.slice(0, cutter-3);
+            return newText+"...";
+        } else if (cutter <=3){
+            let newText = text.slice(0, cutter); 
+            return newText+"..."
+        }
+    } else {
+        return text
+    }
 }
 
 module.exports = trim;
--- src/trimmingString/trimmer.test.js
+++ src/trimmingString/trimmer.test.js
@@ -19,6 +19,18 @@ test("Test if fucntion exists", () => {
     expect(typeof(trim.trimmerString)).toBe("function")
 });
 
-test("Test if fucntion exists", () => {
-    expect(trim.trimmerString("Hey", 2)).toBe("Hey2")
+test("Test algorithm 0", () => {
+    expect(trim.trimmerString("Creating kata is fun", 14)).toBe("Creating ka...")
+});
+
+test("Test algorithm 1", () => {
+    expect(trim.trimmerString("Creating kata is fun", 4)).toBe("C...")
+});
+
+test("Test algorithm 2", () => {
+    expect(trim.trimmerString("He", 1)).toBe("H...")
+});
+
+test("Test algorithm 2", () => {
+    expect(trim.trimmerString("He", 3)).toBe("He")
 });
