2d803446b5c5a63afc5c092127f15221885407a5
PedroB <bereilhp@gmail.com>
2023-02-15 21:51:57 +0100

    Square n sum test and function finished

---
+++ src/Square(n)Sum/squareSum.js
@@ -0,0 +1,9 @@
+let squared = new Object;
+
+squared.summy = function(list){
+    let squaredNums = list.map(x => x**2);
+    let sumOfSquared = squaredNums.reduce((a, b) => a + b, 0);
+    return sumOfSquared;
+}
+
+module.exports = squared;
--- src/Square(n)Sum/squareSum.test.js
+++ src/Square(n)Sum/squareSum.test.js
@@ -1,3 +1,10 @@
+const squared = require("./squareSum")
 test ("Example", () => {
     expect("test").toBe("test");
 });
+
+test("Test #1", () => {
+    expect(squared.summy([1,2,2])).toBe(9)
+})
+
+
