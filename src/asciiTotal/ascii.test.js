f3eaf4cc96d2caf80ebc5d8f0cfae084e4a16e4e
PedroB <bereilhp@gmail.com>
2023-02-19 10:16:21 +0100

    Ascii Total object function total test

--- src/asciiTotal/ascii.test.js
+++ src/asciiTotal/ascii.test.js
@@ -1,5 +1,5 @@
 const Ascii = require("./ascii.js")
-let char = new Ascii;
+let char = new Ascii();
 
 test ("Test class ascii", () => {
     expect(typeof(Ascii)).toBe("function");
@@ -8,4 +8,9 @@ test ("Test class ascii", () => {
 test ("Test object ascii", () => {
     expect(typeof(char)).toBe("object");
 });
+
+test ("Test object ascii function total", () => {
+    expect(typeof(char.total)).toBe("function");
+});
+
 
