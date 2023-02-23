95d987717545d724dec2a95653c6a91d9b76449d
PedroB <bereilhp@gmail.com>
2023-02-23 13:20:28 +0100

    reverseWord function

--- src/reverseWords/reverse.test.js
+++ src/reverseWords/reverse.test.js
@@ -1,3 +1,18 @@
+const Word = require("./reverse.js");
+let words = new Word;
 test ("Example", () => {
     expect("test").toBe("test");
 });
+
+test ("Word class", () => {
+    expect(typeof(Word)).toBe("function");
+});
+
+test ("Word class create words object", () => {
+    expect(typeof(words)).toBe("object");
+});
+
+test ("words function", () => {
+    expect(typeof(words.reverseWord)).toBe("function");
+});
+
