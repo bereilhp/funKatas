e18dfcd666d3a9348366f6fc78b750c0c18dc49c
PedroB <bereilhp@gmail.com>
2023-02-26 16:34:31 +0100

    Array Similar Kata create object function

--- src/ArraySimilar/arrSim.test.js
+++ src/ArraySimilar/arrSim.test.js
@@ -1,7 +1,7 @@
 let Similar = require("./arrSim");
 let array = new Similar();
 
-test ("Test created object workd properly", () => {
+test ("Test created object works properly", () => {
     expect(array).toEqual({});
 });
 
@@ -11,4 +11,8 @@ test ("Test if it is really an object", () => {
 
 test ("Test if the class is a function", () => {
     expect(typeof(Similar)).toEqual("function");
+});
+
+test ("Test if the object has a function", () => {
+    expect(typeof(array.arraySimilar)).toEqual("function");
 });
