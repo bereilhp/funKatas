f88c82f2982a0e1221ab01c4f527d9a2e4ea0e94
PedroB <bereilhp@gmail.com>
2023-02-27 19:25:03 +0100

    Stop Spinning Words function created if word > 5

--- src/spinWords/spin.js
+++ src/spinWords/spin.js
@@ -1,7 +1,14 @@
 class Spin {
     spinner(text){
-        return text;
-    }
+        let counter = 0;
+        let textSplit = text.split(" ");
+        for(let i in textSplit){
+            if(textSplit[i].length > 5){
+                counter++;
+            }
+        }    
+        return counter; 
+    }        
 };
 
 module.exports = Spin;
--- src/spinWords/spin.test.js
+++ src/spinWords/spin.test.js
@@ -4,3 +4,11 @@ let text = new Spin;
 test ("Object text created", () => {
     expect(typeof(text)).toBe("object");
 });
+
+test ("Object has function created", () => {
+    expect(typeof(text.spinner)).toBe("function");
+});
+
+test ("Function spinner", () => {
+    expect(text.spinner("Hey fellow warriors")).toBe(2);
+});
