4ca0e9a2a9bbe12ea77a78cd8c661b71ade6fab8
PedroB <bereilhp@gmail.com>
2023-03-01 18:35:23 +0100

    bit counting function with number 0 return 0 (if condition)

--- src/bitCounting/bitCounting.js
+++ src/bitCounting/bitCounting.js
@@ -2,8 +2,13 @@ class Num {
     bitCounting(number){
         let numToBinary = number.toString(2);
         let binaryMatch = numToBinary.match(/1/g);
-        let matchLength = binaryMatch.length;
-        return matchLength;
+        if (binaryMatch == null){
+            return 0
+        }else {
+            let matchLength = binaryMatch.length;
+            return matchLength;
+        }
+       
     }
 }
 
--- src/bitCounting/bitCounting.test.js
+++ src/bitCounting/bitCounting.test.js
@@ -16,3 +16,7 @@ test ("Function test 12 to return 2", () => {
 test ("Function test 1234 to return 5", () => {
     expect(number.bitCounting(1234)).toBe(5);
 });
+
+test ("Function test 0 to return 0", () => {
+    expect(number.bitCounting(0)).toBe(0);
+});
