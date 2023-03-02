b4caa99c267c3b0a33f20a3ca6800f82d0a8f712
PedroB <bereilhp@gmail.com>
2023-03-02 15:56:05 +0100

    Descending Order Kata finished

--- src/descendingOrder/desOrder.test.js
+++ src/descendingOrder/desOrder.test.js
@@ -20,3 +20,7 @@ test ("Function test #2", () => {
 test ("Function test #3", () => {
     expect(number.orderInv(123456789)).toBe(987654321);
 });
+
+test ("Function test #4", () => {
+    expect(number.orderInv(321)).toBe(321);
+});
