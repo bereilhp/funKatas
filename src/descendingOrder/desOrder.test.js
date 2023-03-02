9b627b6179622df4a03f84323b6618003b6436c7
PedroB <bereilhp@gmail.com>
2023-03-02 15:48:14 +0100

    Descending Order Kata object function

--- src/descendingOrder/desOrder.test.js
+++ src/descendingOrder/desOrder.test.js
@@ -4,3 +4,7 @@ let number = new Num();
 test ("Object number", () => {
     expect(typeof(number)).toBe("object");
 });
+
+test ("Object number", () => {
+    expect(typeof(number.orderInv)).toBe("function");
+});
