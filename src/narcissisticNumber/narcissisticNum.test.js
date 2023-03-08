d9d11fbfbbb17144bbe1bf99523a7fa6bf498033
PedroB <bereilhp@gmail.com>
2023-03-08 09:30:09 +0100

    narcissistic Number function working with non narcissistic number

--- src/narcissisticNumber/narcissisticNum.test.js
+++ src/narcissisticNumber/narcissisticNum.test.js
@@ -9,6 +9,10 @@ test ("Function created", () => {
     expect(typeof(num.narcissitic)).toBe("function");
 });
 
-test ("Function test", () => {
+test ("Function test 153", () => {
     expect(num.narcissitic(153)).toBe(153);
+});
+
+test ("Function test 1652", () => {
+    expect(num.narcissitic(1652)).toBe(1938);
 });
