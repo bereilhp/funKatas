25971c9db558e383391b5553e0e350f93dfe119e
PedroB <bereilhp@gmail.com>
2023-03-08 09:32:34 +0100

    narcissistic Number function testing random nums

--- src/narcissisticNumber/narcissisticNum.test.js
+++ src/narcissisticNumber/narcissisticNum.test.js
@@ -16,3 +16,13 @@ test ("Function test 153", () => {
 test ("Function test 1652", () => {
     expect(num.narcissitic(1652)).toBe(false);
 });
+
+test ("Function test 7", () => {
+    expect(num.narcissitic(7)).toBe(true);
+});
+
+test ("Function test 122", () => {
+    expect(num.narcissitic(122)).toBe(false);
+});
+
+
