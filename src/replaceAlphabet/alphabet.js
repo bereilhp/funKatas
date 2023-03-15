9822e4e06ee7da030f042aa8d370213aec8d953d
PedroB <bereilhp@gmail.com>
2023-03-15 12:25:35 +0100

    Replace with Alphabet Position Kata Finished

--- src/replaceAlphabet/alphabet.js
+++ src/replaceAlphabet/alphabet.js
@@ -1,24 +1,23 @@
 class Alphabet {
-
-    abecedario(){
+    abecedario() {
         let abc = "0abcdefghijklmnopqrstuvwxyz"
         let splitABC = abc.split("");
         return splitABC;
     }
 
-    alphabetPosition(string){
-        let splitString = string.split(""); 
-        let ans = []; 
+    alphabetPosition(string) {
+        let splitString = string.split("");
+        let ans = [];
         let abc = this.abecedario();
 
         splitString.forEach(letra => {
             abc.forEach(abece => {
-                if(letra.toLowerCase() == abece){
+                if (letra.toLowerCase() == abece) {
                     ans.push(abc.indexOf(abece))
                 };
             });
         });
-
+        
         return ans.join(" ")
     }
 }
