77b4646457beefdf989b7ef218ce6e3a3ca55293
PedroB <bereilhp@gmail.com>
2023-03-16 08:35:30 +0100

    Count the Islands Kata Finished

--- src/countTheIslands/islands.js
+++ src/countTheIslands/islands.js
@@ -1,6 +1,23 @@
 class Island {
-    analyzer (matrix) {
-        return matrix;
+    splitMatrix(matrix) {
+        let splitter = matrix
+        return splitter
+
+    }
+
+    analyzer(matrix) {
+        let split = this.splitMatrix(matrix);
+        let ans = []
+        split.forEach(element => {
+            if (element == 0) {
+                ans.push("~");
+            } else
+                ans.push("X");
+        });
+        
+        let stringy = ans.join("")
+
+        return stringy;
     };
 };
 
--- src/countTheIslands/islands.test.js
+++ src/countTheIslands/islands.test.js
@@ -9,7 +9,19 @@ test ("Create object island", () => {
     expect(typeof(island)).toBe("object");
 });
 
-test ("Create object island", () => {
+test ("Create object function analyzer", () => {
     expect(typeof(island.analyzer)).toBe("function");
 });
+
+test ("Testing function Analyzer", () => {
+    let islands = [0,0,0,0,0,0,0,0,0,0];          
+    expect(island.analyzer(islands)).toBe("~~~~~~~~~~");
+});
+
+test ("Testing function Analyzer", () => {
+    let islands = [0,0,1,0,1,0,1,0,0,0];          
+    expect(island.analyzer(islands)).toBe("~~X~X~X~~~");
+});
+
+
 
