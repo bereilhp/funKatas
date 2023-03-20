c3bb5b17e756ae44bbe4166f82860edbed48d168
PedroB <bereilhp@gmail.com>
2023-03-20 19:36:31 +0100

    Prime Number Kata create function is_prime test 1 and 7

--- src/primeNumber/prime.test.js
+++ src/primeNumber/prime.test.js
@@ -8,10 +8,18 @@ test ("Type prime.is_prime", () => {
     expect(typeof(prime.is_prime)).toBe("function");
 });
 
-test ("prime.is_prime test 2", () => {
+test ("prime.is_prime test 6", () => {
     expect(prime.is_prime(6)).toBe(false);
 });
 
-test ("prime.is_prime test 2", () => {
+test ("prime.is_prime test 0", () => {
     expect(prime.is_prime(0)).toBe(false);
+});
+
+test ("prime.is_prime test 1", () => {
+    expect(prime.is_prime(1)).toBe(false);
+});
+
+test ("prime.is_prime test 7", () => {
+    expect(prime.is_prime(7)).toBe(true);
 });
