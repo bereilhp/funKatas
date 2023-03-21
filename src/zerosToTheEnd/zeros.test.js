45f36c1b290c17551e129933098a955df5dd57ee
PedroB <bereilhp@gmail.com>
2023-03-21 08:15:12 +0100

    Zeros To The End Kata test function moveZeros

--- src/zerosToTheEnd/zeros.test.js
+++ src/zerosToTheEnd/zeros.test.js
@@ -7,3 +7,7 @@ test ("typeof num", () => {
 test ("typeof num.moveZeros", () => {
     expect(typeof(num.moveZeros)).toBe("function");
 });
+
+test ("num.moveZeros simplest test", () => {
+    expect(num.moveZeros([0,0,0,0])).toEqual([0,0,0,0]);
+});
