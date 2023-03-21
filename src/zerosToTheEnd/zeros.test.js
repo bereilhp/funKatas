ca609a0f4e8b68a0174275e12b843132ce89a5ae
PedroB <bereilhp@gmail.com>
2023-03-21 08:14:06 +0100

    Zeros To The End Kata create function moveZeros and test

--- src/zerosToTheEnd/zeros.test.js
+++ src/zerosToTheEnd/zeros.test.js
@@ -3,3 +3,7 @@ const num = require("./zeros");
 test ("typeof num", () => {
     expect(typeof(num)).toBe("object");
 });
+
+test ("typeof num.moveZeros", () => {
+    expect(typeof(num.moveZeros)).toBe("function");
+});
