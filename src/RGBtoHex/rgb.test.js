a3d76aad82c4daec49eff6a2102304b7127f4afd
PedroB <bereilhp@gmail.com>
2023-03-23 11:36:48 +0100

    create function rgb

--- src/RGBtoHex/rgb.test.js
+++ src/RGBtoHex/rgb.test.js
@@ -3,3 +3,7 @@ const conversor = require("./rgb.js");
 test ("typeof conversor", () => {
     expect(typeof(conversor)).toBe("object");
 });
+
+test ("typeof conversor.rgb", () => {
+    expect(typeof(conversor.rgb)).toBe("function");
+});
