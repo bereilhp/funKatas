817c98f854eeeb264dc42c916753a860376ce3c2
PedroB <bereilhp@gmail.com>
2023-03-23 12:08:25 +0100

    Test 9400D3

--- src/RGBtoHex/rgb.test.js
+++ src/RGBtoHex/rgb.test.js
@@ -8,12 +8,16 @@ test ("typeof conversor.rgb", () => {
     expect(typeof(conversor.rgb)).toBe("function");
 });
 
-test ("typeof conversor.rgb", () => {
+test ("typeof conversor.rgb FFFFFF", () => {
     expect(conversor.rgb(255,255,255)).toBe("FFFFFF");
 });
 
-test ("typeof conversor.rgb", () => {
+test ("typeof conversor.rgb 000000", () => {
     expect(conversor.rgb(0,0,0)).toBe("000000");
+});
+
+test ("typeof conversor.rgb 9400D3", () => {
+    expect(conversor.rgb(148,0,211)).toBe("9400D3");
 });
 
 
