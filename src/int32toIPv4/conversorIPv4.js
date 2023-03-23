21178232bd54387a0b84c0e4a41c1ed384e39aa3
PedroB <bereilhp@gmail.com>
2023-03-23 07:07:16 +0100

    Test ipv4 conversor big num

--- src/int32toIPv4/conversorIPv4.js
+++ src/int32toIPv4/conversorIPv4.js
@@ -1,6 +1,19 @@
 const conversor = {
     ipv4 : function(num){
-        return num;
+        let numToBin = num.toString(2);
+        let newArr = numToBin.split("");
+        
+        let splitArr32 = newArr.slice(0,8).join("");
+        let splitArr24 = newArr.slice(8,16).join("");
+        let splitArr16 = newArr.slice(16,24).join("");
+        let splitArr8 = newArr.slice(24,32).join("");
+
+        let num32 = parseInt(splitArr32,2);
+        let num24 = parseInt(splitArr24,2);
+        let num16 = parseInt(splitArr16,2);
+        let num8 = parseInt(splitArr8,2);
+
+        return `${num32}.${num24}.${num16}.${num8}`;
     }
 }
 
--- src/int32toIPv4/conversorIPv4.test.js
+++ src/int32toIPv4/conversorIPv4.test.js
@@ -7,3 +7,7 @@ test ("typeof conversor", () => {
 test ("typeof conversor.ipv4", () => {
     expect(typeof(conversor.ipv4)).toBe("function");
 });
+
+test ("Test ipv4 conversor", () => {
+    expect(conversor.ipv4(2149583361)).toBe("128.32.10.1");
+});
