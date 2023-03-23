91ec3a0fa57a193fda00d24c476c6ce4bc7ad9d4
PedroB <bereilhp@gmail.com>
2023-03-23 06:46:32 +0100

    create function ipv4

--- src/int32toIPv4/conversorIPv4.test.js
+++ src/int32toIPv4/conversorIPv4.test.js
@@ -3,3 +3,7 @@ const conversor = require("./conversorIPv4");
 test ("typeof conversor", () => {
     expect(typeof(conversor)).toBe("object");
 });
+
+test ("typeof conversor.ipv4", () => {
+    expect(typeof(conversor.ipv4)).toBe("function");
+});
