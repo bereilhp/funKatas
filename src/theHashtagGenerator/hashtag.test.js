81d5f13862718fc33c7186812a73ef0dee40e8a9
PedroB <bereilhp@gmail.com>
2023-04-10 16:24:44 +0200

    Test typeof hashtag function

--- src/theHashtagGenerator/hashtag.test.js
+++ src/theHashtagGenerator/hashtag.test.js
@@ -3,3 +3,7 @@ const { hashtag } = require("./hashtag");
 test ("Tyepof Hashtag", () => {
     expect(typeof(hashtag)).toBe("object");
 });
+
+test ("Tyepof Hashtag function", () => {
+    expect(typeof(hashtag.hashtagGenerator)).toBe("function");
+});
