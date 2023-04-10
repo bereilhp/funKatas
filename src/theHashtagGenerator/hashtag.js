696eb165128660871988e74ebcaeb7153e898aec
PedroB <bereilhp@gmail.com>
2023-04-10 16:37:49 +0200

    Test sentence with capitalize

--- src/theHashtagGenerator/hashtag.js
+++ src/theHashtagGenerator/hashtag.js
@@ -1,6 +1,12 @@
 const hashtag = {
     hashtagGenerator : function(text) {
-        return "#" + text;
+        let splitText = text.split(" ");
+
+        for (let i = 1; i < splitText.length; i++) {
+            splitText[i] = splitText[i].charAt(0).toUpperCase() + splitText[i].slice(1);
+        }
+        
+        return "#" + splitText.join("");
     }
 };
 
--- src/theHashtagGenerator/hashtag.test.js
+++ src/theHashtagGenerator/hashtag.test.js
@@ -8,6 +8,10 @@ test ("Tyepof Hashtag function", () => {
     expect(typeof(hashtag.hashtagGenerator)).toBe("function");
 });
 
-test ("Testing function simple hello ", () => {
+test ("Testing function simple hello", () => {
     expect(hashtag.hashtagGenerator("hello")).toBe("#hello");
+});
+
+test ("Testing function sentence", () => {
+    expect(hashtag.hashtagGenerator("hello world")).toBe("#helloWorld");
 });
