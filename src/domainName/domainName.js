d51acfd3e56b8d21f685b38b40a0fb31027c9bd3
PedroB <bereilhp@gmail.com>
2023-04-11 15:50:35 +0200

    Domain Name Kata Finished

--- src/domainName/domainName.js
+++ src/domainName/domainName.js
@@ -1,9 +1,10 @@
 const domain = {
     domainName : function(link){
+        
         if(!(link.startsWith("http://") || link.startsWith("https://"))){
             link = "http://" + link;
         }
-        
+
         let myURL = new URL(link);
         let hostname = myURL.hostname;
         let splitHostname = hostname.split(".");
