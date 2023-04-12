21ac7fc534fc44b3172133107487654a153378ee
PedroB <bereilhp@gmail.com>
2023-04-12 18:47:01 +0200

    Test typeof password

---
+++ src/regexPassword/passwordValidator.js
@@ -0,0 +1,7 @@
+const password = {
+    validator : function(pass){
+        return pass;
+    }
+};
+
+module.exports = {password};
--- src/regexPassword/passwordValidator.test.js
+++ src/regexPassword/passwordValidator.test.js
@@ -1,3 +1,5 @@
-test ("Example", () => {
-    expect("test").toBe("test");
+const {password} = require("./passwordValidator.js");
+
+test ("Test typeof password", () => {
+    expect(typeof(password)).toBe("object");
 });
