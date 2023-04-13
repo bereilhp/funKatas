0cbcaddd9b91685b1b0b83994e355077874d9cae
PedroB <bereilhp@gmail.com>
2023-04-13 16:07:26 +0200

    Last Digit simple test 4^1

--- src/lastDigitOfLargeNum/lastDigitNum.test.js
+++ src/lastDigitOfLargeNum/lastDigitNum.test.js
@@ -11,3 +11,7 @@ test ("Typeof num.lastDigit", () => {
 test ("num.lastDigit simple 0^0", () => {
     expect(num.lastDigit(0,0)).toBe(1);
 });
+
+test ("num.lastDigit simple 4^1", () => {
+    expect(num.lastDigit(4,1)).toBe(4);
+});
