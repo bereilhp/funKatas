9d20b607fb83fb21256119bcd610bdc80fee45e8
PedroB <bereilhp@gmail.com>
2023-04-13 16:05:54 +0200

    Last Digit simple test 0^0

--- src/lastDigitOfLargeNum/lastDigitNum.test.js
+++ src/lastDigitOfLargeNum/lastDigitNum.test.js
@@ -7,3 +7,7 @@ test ("Typeof num", () => {
 test ("Typeof num.lastDigit", () => {
     expect(typeof(num.lastDigit)).toBe("function");
 });
+
+test ("num.lastDigit simple 0^0", () => {
+    expect(num.lastDigit(0,0)).toBe(1);
+});
