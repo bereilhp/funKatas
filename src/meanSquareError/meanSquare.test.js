c74311e90ad05030bffb28a6eee2383047fa83e9
PedroB <bereilhp@gmail.com>
2023-04-22 17:47:24 +0200

    Mean Square Error function test 2

--- src/meanSquareError/meanSquare.test.js
+++ src/meanSquareError/meanSquare.test.js
@@ -14,3 +14,7 @@ test ("meanSqr.meanSqrError 1", () => {
 test ("meanSqr.meanSqrError 2", () => {
     expect(meanSqr.meanSqrError([10, 20, 10, 2], [10, 25, 5, -2])).toBe(16.5);
 });
+
+test ("meanSqr.meanSqrError 3", () => {
+    expect(meanSqr.meanSqrError([-1, 0], [0, -1])).toBe(1);
+});
