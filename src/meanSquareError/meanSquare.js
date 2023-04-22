25b42de7509efd670752c00500ae2a92f6cad6d0
PedroB <bereilhp@gmail.com>
2023-04-22 17:40:27 +0200

    Mean Square Error function working and test

--- src/meanSquareError/meanSquare.js
+++ src/meanSquareError/meanSquare.js
@@ -1,6 +1,16 @@
 const meanSqr = {
     meanSqrError: function(firstArr, secondArr){
-        return firstArr + secondArr;
+        let arrayAns = [];
+        for(let i = 0; i < firstArr.length; i++){
+            let ans = secondArr[i] - firstArr[i];
+            arrayAns.push(ans);
+        }
+
+        let mapArrayAns = arrayAns.map((e) => e**2);
+        let addyMap = mapArrayAns.reduce((a,b) => a+b, 0);
+        let divAddyMap = addyMap/3;
+
+        return divAddyMap;
     }
 };
 
--- src/meanSquareError/meanSquare.test.js
+++ src/meanSquareError/meanSquare.test.js
@@ -6,3 +6,7 @@ test ("test meanSqr object", () => {
 test ("test meanSqr.meanSqrError function", () => {
     expect(typeof(meanSqr.meanSqrError)).toBe("function");
 });
+
+test ("meanSqr.meanSqrError 1", () => {
+    expect(meanSqr.meanSqrError([1,2,3], [4,5,6])).toBe(9);
+});
