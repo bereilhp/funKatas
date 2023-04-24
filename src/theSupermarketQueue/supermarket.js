88964285fb624f893a6de7bb68f5dd1512d16110
PedroB <bereilhp@gmail.com>
2023-04-24 16:14:25 +0200

    Function queueTime test 2

--- src/theSupermarketQueue/supermarket.js
+++ src/theSupermarketQueue/supermarket.js
@@ -2,6 +2,11 @@ const queue = {
     queueTime : function(customers, n){
         if(n === 1 ){
             return customers.reduce((a,b) => a+b,0);
+        }else if(n === 2){
+            let arrayWithoutFirstElement = customers.slice(1);
+            if(customers[0] > arrayWithoutFirstElement.reduce((a,b) => a+b,0)){
+                return customers[0];
+            }
         }
     }
 };
--- src/theSupermarketQueue/supermarket.test.js
+++ src/theSupermarketQueue/supermarket.test.js
@@ -11,3 +11,8 @@ test ("function queueTime exists", () => {
 test ("function queueTime test #1", () => {
     expect(queue.queueTime([5,3,4], 1)).toBe(12);
 });
+
+test ("function queueTime test #2", () => {
+    expect(queue.queueTime([10,2,3,3], 2)).toBe(10);
+});
+
