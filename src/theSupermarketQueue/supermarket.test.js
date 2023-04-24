84b02db859f4d6def4bef1f24d5b9797f56fa7fb
PedroB <bereilhp@gmail.com>
2023-04-24 16:08:28 +0200

    Function queueTime exists

--- src/theSupermarketQueue/supermarket.test.js
+++ src/theSupermarketQueue/supermarket.test.js
@@ -3,3 +3,7 @@ const {queue} = require("./supermarket");
 test ("object queue exists", () => {
     expect(typeof(queue)).toBe("object");
 });
+
+test ("function queueTime exists", () => {
+    expect(typeof(queue.queueTime)).toBe("function");
+});
