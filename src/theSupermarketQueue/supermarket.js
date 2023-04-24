81222bf7979ecd1f0cd1feda6f36912bc55cd5b5
PedroB <bereilhp@gmail.com>
2023-04-24 16:24:39 +0200

    Format code

--- src/theSupermarketQueue/supermarket.js
+++ src/theSupermarketQueue/supermarket.js
@@ -1,5 +1,6 @@
 const queue = {
     queueTime : function(customers, n){
+        
         if(n === 1 ){
             return customers.reduce((a,b) => a+b,0);
         }else if(n === 2){
@@ -9,11 +10,13 @@ const queue = {
             }else {
                 let till1 = 0;
                 let till2 = 0;
-                for(let i = 0; i< customers.length; i++){
+
+                for(let i = 0; i < customers.length; i++){
                     if(i % 2 === 0 ){
                         till1 = till1 + customers[i];
                     }else till2 = till2 + customers[i];
                 }
+
                 let ans = Math.max(till1, till2);
                 return ans;
             }
