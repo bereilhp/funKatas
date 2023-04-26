58548b4e5af59a113bd4badaffeea91269920e4d
PedroB <bereilhp@gmail.com>
2023-04-26 17:53:27 +0200

    Testin primeNumber function test with 0

--- src/primer/primer.js
+++ src/primer/primer.js
@@ -3,8 +3,8 @@ class num {
         try {
             if(typeof(number) != "number"){
                 throw new Error ("Not a Number");
-            }else if(number < 0){
-                throw "Negative Number";
+            }else if(number <= 0){
+                throw "Not valid Number";
             }
         } catch (error) {
             return error.toString();
--- src/primer/primer.test.js
+++ src/primer/primer.test.js
@@ -13,15 +13,18 @@ describe("Object and Function exists", () => {
 
 describe("Testing function primeNumber input",() =>{
     let numObj = new num();
-    test ("Function simple test", () => {
-        expect(numObj.primeNumber(10)).toBe(10);
-    });
-
     it("Function throw Error when negative test", () => {
-        expect(numObj.primeNumber(-1)).toBe("Negative Number");
+        expect(numObj.primeNumber(-1)).toBe("Not valid Number");
     });
 
     it("Function throw Error when not a number", () => {
         expect(numObj.primeNumber("s")).toBe("Error: Not a Number");
+    });
+});
+
+describe("Testing function primeNumber working properly", () => {
+    let numObj = new num();
+    it("PrimeNumber Function test 0", () => {
+        expect(numObj.primeNumber(0)).toBe("Not valid Number");
     });
 });
