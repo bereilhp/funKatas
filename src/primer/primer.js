d1482c1790fb498d59744c0f8e3045a594704f9f
PedroB <bereilhp@gmail.com>
2023-04-30 16:38:38 +0200

    Format Code and move loop into try block

--- src/primer/primer.js
+++ src/primer/primer.js
@@ -7,25 +7,26 @@ class num {
      * @param {Int} number Any  positive number bigger than zero
      * @returns true or false if number is prime
      */
-    primeNumber(number){
+    primeNumber(number) {
         try {
-            if(typeof(number) != "number"){
-                throw new Error ("Not a Number");
-            }else if(number <= 1){
+            let count = 0;
+
+            if (typeof (number) != "number") {
+                throw new Error("Not a Number");
+            } else if (number <= 1) {
                 throw "Not valid Number";
+            } else {
+                for (let i = 1; i <= number; i++) {
+                    if (number % i === 0) {
+                        count++;
+                    }
+                }
             }
+            return count === 2;
+
         } catch (error) {
             return error.toString();
         }
-
-        let count = 0;
-        for(let i = 1; i <= number; i++){
-            if(number % i === 0){
-                count++;
-            }
-        }
-
-        return count === 2;
     }
 }
 
