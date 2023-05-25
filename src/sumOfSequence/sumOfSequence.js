95e5cb2d588b77ea2bf290c326146d8984ce34f9
PedroB <bereilhp@gmail.com>
2023-05-25 12:33:16 +0200

    Simple test begin === end

--- src/sumOfSequence/sumOfSequence.js
+++ src/sumOfSequence/sumOfSequence.js
@@ -2,6 +2,8 @@ class Sequence {
     sum(begin,end,step){
         if(begin > end){
             return 0;
+        } else if (begin === end){
+            return begin;
         }
     }
 }
--- src/sumOfSequence/sumOfSequence.test.js
+++ src/sumOfSequence/sumOfSequence.test.js
@@ -10,4 +10,9 @@ describe("Simple test function", () => {
         const sequence = new Sequence;
         expect(sequence.sum(2,1,2)).toBe(0);
     });
+
+    it("Simple test of function sum end and begin equal", () => {
+        const sequence = new Sequence;
+        expect(sequence.sum(2,2,2)).toBe(2);
+    });
 });
