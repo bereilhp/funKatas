c78835a4da6a4f4a28387d48286eebbac5f2378a
PedroB <bereilhp@gmail.com>
2023-05-27 13:49:25 +0200

    invertValues obj exists

--- src/invertValues/invertValues.test.js
+++ src/invertValues/invertValues.test.js
@@ -4,4 +4,9 @@ describe("Test Class, Obj and Function Exists", () => {
     it("Class InvertValues Exists", () => {
         expect(typeof(InvertValues)).toBe("function");
     });
+
+    it("Obj InvertValues Exists", () => {
+        const invertValues = new InvertValues();
+        expect(typeof(invertValues)).toBe("object");
+    });
 });
