988cafa31b5b7bd4cd9e1a5bb1401eb44c055096
PedroB <bereilhp@gmail.com>
2023-05-29 12:15:33 +0200

    simple tests

--- src/indexOfElementArray/index.test.js
+++ src/indexOfElementArray/index.test.js
@@ -2,3 +2,7 @@ const {find} = require("./index");
 test ("Simple test", () => {
     expect(find([1,2,3], 2)).toBe(1);
 });
+
+test ("Simple test 2", () => {
+    expect(find([1,2,3], 4)).toBe("Not found");
+});
