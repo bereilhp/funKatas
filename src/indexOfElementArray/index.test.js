de9eeba110ad18415f8a9380c0a99545de73547a
PedroB <bereilhp@gmail.com>
2023-05-29 12:15:50 +0200

    simple test 3

--- src/indexOfElementArray/index.test.js
+++ src/indexOfElementArray/index.test.js
@@ -6,3 +6,7 @@ test ("Simple test", () => {
 test ("Simple test 2", () => {
     expect(find([1,2,3], 4)).toBe("Not found");
 });
+
+test ("Simple test 3", () => {
+    expect(find([1,2,3], 1)).toBe(0);
+});
