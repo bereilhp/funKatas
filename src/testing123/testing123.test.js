d276d024bbd23938a8082ae24d61115679348b18
PedroB <bereilhp@gmail.com>
2023-06-04 15:15:47 +0200

    Object exists

--- src/testing123/testing123.test.js
+++ src/testing123/testing123.test.js
@@ -4,4 +4,9 @@ describe("Check if they exist", () => {
     it("Class exists", () => {
         expect(typeof(LineNumber)).toBe("function");
     });
+
+    it("Object exists", () => {
+        const lineNumber = new LineNumber();
+        expect(typeof(lineNumber)).toBe("object");
+    });
 });
