2297068fa1dd02dce74570400f59267ec4b9e870
PedroB <bereilhp@gmail.com>
2023-06-04 15:23:53 +0200

    Simple test

--- src/testing123/testing123.js
+++ src/testing123/testing123.js
@@ -1,10 +1,15 @@
 class LineNumber {
-    count(array){
-        if(array.length == 0){
+    count(array) {
+        if (array.length == 0) {
             return array;
+        } else {
+            let ans = [];
+            for (let i = 0; i < array.length; i++) {
+                ans.push(`${i + 1}: ${array[i]}`);
+            }
+            return ans;
         }
     }
-
 }
 
 module.exports = {
--- src/testing123/testing123.test.js
+++ src/testing123/testing123.test.js
@@ -1,18 +1,18 @@
-const {LineNumber} = require("./testing123");
+const { LineNumber } = require("./testing123");
 
 describe("Check if they exist", () => {
     it("Class exists", () => {
-        expect(typeof(LineNumber)).toBe("function");
+        expect(typeof (LineNumber)).toBe("function");
     });
 
     it("Object exists", () => {
         const lineNumber = new LineNumber();
-        expect(typeof(lineNumber)).toBe("object");
+        expect(typeof (lineNumber)).toBe("object");
     });
 
     it("Function exists", () => {
         const lineNumber = new LineNumber();
-        expect(typeof(lineNumber.count)).toBe("function");
+        expect(typeof (lineNumber.count)).toBe("function");
     });
 });
 
@@ -20,5 +20,10 @@ describe("test function count", () => {
     it("Simple empty test", () => {
         const lineNumber = new LineNumber();
         expect(lineNumber.count([])).toEqual([]);
+    });
+
+    it("Simple test", () => {
+        const lineNumber = new LineNumber();
+        expect(lineNumber.count(["a", "b", "c"])).toEqual(["1: a", "2: b", "3: c"]);
     });
 });
