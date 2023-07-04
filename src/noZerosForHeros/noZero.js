3976efd2c1165bae5e0294ff6e4356ffc6518c6d
PedroB <bereilhp@gmail.com>
2023-07-04 12:06:20 +0200

    Function test 1

--- src/noZerosForHeros/noZero.js
+++ src/noZerosForHeros/noZero.js
@@ -1,6 +1,8 @@
 class Zero {
-    removeZeros(){
-        return ;
+    removeZeros(num){
+        if(num === 0){
+            return 0;
+        } else return Number(String(num).replaceAll("0",""));
     }
 
 }
--- src/noZerosForHeros/noZero.test.js
+++ src/noZerosForHeros/noZero.test.js
@@ -15,3 +15,10 @@ describe("Test if they exist", () => {
         expect(typeof(zero.removeZeros)).toBe("function");
     });
 });
+
+describe("Function tests", () => {
+    it("Function Test #1", () => {
+        let zero = new Zero();
+        expect(zero.removeZeros(100)).toBe(1);
+    });
+});
