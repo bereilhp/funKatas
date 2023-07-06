0039941eac3548ab5b62b24ebc0e4fd7d367d491
PedroB <bereilhp@gmail.com>
2023-07-06 15:28:23 +0200

    Object exists test

--- src/reverseListOrder/reverseList.test.js
+++ src/reverseListOrder/reverseList.test.js
@@ -4,4 +4,9 @@ describe("Test if they exist", () => {
     it("Class exists", () => {
         expect(typeof(Reverse)).toBe("function");
     });
+
+    it("Object exists", () => {
+        const reverse = new Reverse();
+        expect(typeof(reverse)).toBe("object");
+    });
 });
