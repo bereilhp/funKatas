ce92ed82860bc56025fb296620735ad3e4346613
PedroB <bereilhp@gmail.com>
2023-11-20 17:54:33 +0100

    Shuffle it Up

--- src/anExampleToTDD/adders.js
+++ src/anExampleToTDD/adders.js
@@ -1,18 +1,16 @@
-function add(a,b){
-    try{
-        if(typeof(a) == "number" && typeof(b)== "number"){
-            return a+b; 
-        }else 
-            throw "Not a number";
-    }catch(e){
-        return e;
-    }
+function add(a, b) {
+  try {
+    if (typeof a == "number" && typeof b == "number") {
+      return a + b;
+    } else throw "Not a number";
+  } catch (e) {
+    return e;
+  }
 }
 
-function testa(){
-    let x = add(4,5);
-    return x;
-
+function testa() {
+  let x = add(4, 5);
+  return x;
 }
 
 module.exports = { add, testa };
--- src/anExampleToTDD/adders.test.js
+++ src/anExampleToTDD/adders.test.js
@@ -1,30 +1,28 @@
 const { add, testa } = require("./adders.js");
 
 test("Function exist", () => {
-    expect(typeof(add)).toBe("function"); 
-    //create test that test if function exist see it fail, then create function see it pass 
-}); 
-
-test("Test #1 - add two numbers",() => {
-    expect(add(1,2)).toBe(3);
+  expect(typeof add).toBe("function");
+  //create test that test if function exist see it fail, then create function see it pass
 });
 
-test("Test #1 - add one letter and one number",() => {
-    expect(add("3",2)).toBe("Not a number");
-}); 
-test("Test #1 - add one string of characters and a number",() => {
-    expect(add("hola",2)).toBe("Not a number");
-}); 
+test("Test #1 - add two numbers", () => {
+  expect(add(1, 2)).toBe(3);
+});
 
-test("Test #1 - add a number and a string of characters",() => {
-    expect(add(2,"hola")).toBe("Not a number");
-}); 
-test("Test #1 - add two strings together",() => {
-    expect(add("hola","hola")).toBe("Not a number");
-}); 
+test("Test #1 - add one letter and one number", () => {
+  expect(add("3", 2)).toBe("Not a number");
+});
+test("Test #1 - add one string of characters and a number", () => {
+  expect(add("hola", 2)).toBe("Not a number");
+});
 
-test("Testa",() => {
-    expect(testa()).toBe(9);
-}); 
+test("Test #1 - add a number and a string of characters", () => {
+  expect(add(2, "hola")).toBe("Not a number");
+});
+test("Test #1 - add two strings together", () => {
+  expect(add("hola", "hola")).toBe("Not a number");
+});
 
-
+test("Testa", () => {
+  expect(testa()).toBe(9);
+});
