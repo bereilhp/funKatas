2e05cdcf5ea0badba7aea06b034a43c0f87926d2
PedroB <bereilhp@gmail.com>
2023-11-22 17:19:28 +0100

    Shuffle test 1 and 2

--- src/shuffleItUp/shuffle.js
+++ src/shuffleItUp/shuffle.js
@@ -1,3 +1,11 @@
-const permutation = () => {};
+const permutation = (array) => {
+  const numElementsArray = array.length;
+
+  if (numElementsArray === 0 || numElementsArray === 1) {
+    return 1;
+  } else {
+    return numElementsArray * permutation(array.slice(0, numElementsArray - 1));
+  }
+};
 
 module.exports = { permutation };
--- src/shuffleItUp/shuffle.test.js
+++ src/shuffleItUp/shuffle.test.js
@@ -4,4 +4,12 @@ describe("Tests", () => {
   it("typeof permutation", () => {
     expect(typeof permutation).toBe("function");
   });
+
+  it("permutation #1", () => {
+    expect(permutation([1, 2])).toEqual(2);
+  });
+
+  it("permutation #2", () => {
+    expect(permutation([1, 2, 3])).toEqual(6);
+  });
 });
