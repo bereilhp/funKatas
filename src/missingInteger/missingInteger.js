4ffb27cc0c82a78c390a9f8135a848db259586e2
PedroB <bereilhp@gmail.com>
2025-10-13 15:46:08 +0200

    Fix for 2 spaces indent

--- src/missingInteger/missingInteger.js
+++ src/missingInteger/missingInteger.js
@@ -1,12 +1,12 @@
 function solution(A) {
-    const seen = new Set(A);
-    let smallest = 1;
-    
-    while (seen.has(smallest)) {
-        smallest++;
-    }
-    
-    return smallest;
+  const seen = new Set(A);
+  let smallest = 1;
+  
+  while (seen.has(smallest)) {
+    smallest++;
+  }
+  
+  return smallest;
 }
 
 module.exports = solution
